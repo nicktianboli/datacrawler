2334a3b9feb460c23e46173a73e0e53072095339
nicktianboli <li_tianbo@live.com>
2018-01-04 19:10:35 +0800

    commit

--- twitter.py
+++ twitter.py
@@ -4,4 +4,3 @@ api = twitter.Api(consumer_key='xITCo7ggiRehaEiWP1Oy23Sfl',
                   consumer_secret='GjCsrgTbPKPeWXUIwFzHvrdXrCLur3en67awxHbHVBD5u4f0XP',
                   access_token_key='2369848692-kcMAn9RySj6eeJ21dVgEN7Ul94nvo5Y6Wf1AVdI',
                   access_token_secret='gxVuPmIWgY26Dkl3JT6l5bdWob8l3HSMZAKCdCjsddrSI')
-as
