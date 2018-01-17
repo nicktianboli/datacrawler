1b4480225afda0f5eb535476302f2c4223fa0250
nicktianboli <li_tianbo@live.com>
2018-01-17 21:19:32 +0800

    twitter

--- twitter.py
+++ twitter.py
@@ -4,3 +4,5 @@ api = twitter.Api(consumer_key='xITCo7ggiRehaEiWP1Oy23Sfl',
                   consumer_secret='GjCsrgTbPKPeWXUIwFzHvrdXrCLur3en67awxHbHVBD5u4f0XP',
                   access_token_key='2369848692-kcMAn9RySj6eeJ21dVgEN7Ul94nvo5Y6Wf1AVdI',
                   access_token_secret='gxVuPmIWgY26Dkl3JT6l5bdWob8l3HSMZAKCdCjsddrSI')
+
+results = api.GetSearch(raw_query="q=USA%20&result_type=recent&count=100")
